d08648dd9701da9094f5b49c1bb86ba18d9a4c97
ilgiz <ilgiz.yanuzakov@rossko.ru>
2017-03-21 00:05:06 +0500

    formatting

--- 03-lcd/lcd0.h
+++ 03-lcd/lcd0.h
@@ -18,7 +18,7 @@ LCD Macro Defines
 #define LCD_D6			P7.BIT.bit2	
 #define LCD_D7			P7.BIT.bit3	
 
-#define LCD_DATA_PORT	P7.p7
+#define LCD_DATA_PORT		P7.p7
 
 /* Bit mask from entire port */	 	
 #define DATA_PORT_MASK		0x0F
@@ -33,8 +33,8 @@ LCD Macro Defines
 #define DATA_WR 		1
 #define CTRL_WR 		0
 
-#define SET_BIT_HIGH 	1
-#define SET_BIT_LOW 	0
+#define SET_BIT_HIGH		1
+#define SET_BIT_LOW		0
 
 /* Set to ensure base delay */
 #define DELAY_TIMING		0x04
@@ -44,41 +44,41 @@ LCD Macro Defines
 /* Maximum characters per line of LCD display. */	
 #define MAXIMUM_LINES		4		
 
-#define LCD_LINE1 0
-#define LCD_LINE2 16
-#define LCD_LINE3 8
-#define LCD_LINE4 24
+#define LCD_LINE1			0
+#define LCD_LINE2			16
+#define LCD_LINE3			8
+#define LCD_LINE4			24
 
 /* Clear LCD display and home cursor */
-#define LCD_CLEAR        0x01
+#define LCD_CLEAR			0x01
 /* move cursor to line 1 */
-#define LCD_HOME_L1      0x80
+#define LCD_HOME_L1			0x80
 /* move cursor to line 2 */      
-#define LCD_HOME_L2      0xC0
+#define LCD_HOME_L2			0xC0
 /* move cursor to line 3 */      
-#define LCD_HOME_L3      0x94
+#define LCD_HOME_L3			0x94
 /* move cursor to line 4 */      
-#define LCD_HOME_L4      0xD4
+#define LCD_HOME_L4			0xD4
 /* Cursor auto decrement after R/W */  
-#define CURSOR_MODE_DEC  0x04
+#define CURSOR_MODE_DEC		0x04
 /* Cursor auto increment after R/W */
-#define CURSOR_MODE_INC  0x06
+#define CURSOR_MODE_INC		0x06
 /* Setup, 4 bits,2 lines, 5X7 */
-#define FUNCTION_SET     0x28
+#define FUNCTION_SET		0x28
 /* Display ON with Cursor */
-#define LCD_CURSOR_ON    0x0E
+#define LCD_CURSOR_ON		0x0E
 /* Display ON with Cursor off */
-#define LCD_CURSOR_OFF   0x0C
+#define LCD_CURSOR_OFF		0x0C
 /* Display on with blinking cursor */
-#define LCD_CURSOR_BLINK 0x0D
+#define LCD_CURSOR_BLINK		0x0D
 /*Move Cursor Left One Position */
-#define LCD_CURSOR_LEFT  0x10
+#define LCD_CURSOR_LEFT			0x10
 /* Move Cursor Right One Position */
-#define LCD_CURSOR_RIGHT 0x14     
+#define LCD_CURSOR_RIGHT		0x14     
 /* Turn on the display */
-#define LCD_DISPLAY_ON   0x04
+#define LCD_DISPLAY_ON			0x04
 /* Set cursor to start of line 2 */
-#define LCD_TWO_LINE     0x08
+#define LCD_TWO_LINE			0x08
 
 /* LCD intialisation function prototype */
 void InitialiseDisplay(void);
